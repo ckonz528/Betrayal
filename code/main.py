14407a274b94aa17da13644bdab324f1caa71df7
Claire Konz <claire.konz@gmail.com>
2023-01-16 11:39:36 -0500

    Initial commit

---
+++ code/main.py
@@ -0,0 +1,29 @@
+import pygame
+from sys import exit
+import settings as s
+
+
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        self.screen = pygame.display.set_mode((s.SCREEN_W, s.SCREEN_H))
+        pygame.display.set_caption(s.TITLE)
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+            dt = self.clock.tick() / 1000  # delta time
+
+            self.screen.fill('green')
+
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ code/settings.py
@@ -0,0 +1,26 @@
+# import os
+# BASE_PATH = os.getcwd()
+
+
+# def get_path(folder, file): return os.path.join(BASE_PATH, folder, file)
+
+
+# Screen settings
+SCREEN_W = 1280
+SCREEN_H = 720
+TILE_SIZE = 64
+
+TITLE = 'Betrayal at House on a Hill'
+
+BG_IMAGE = '../graphics/red_velvet_bg.jpg'
+
+# image sizes
+# TILE_SIZE = 200
+PLAYER_SIZE = 100
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
