d1b359ac0b7e5488eda72219c06b3df8daa3c92a
Claire Konz <claire.konz@gmail.com>
2023-01-17 14:43:40 -0500

    added frequently used paths

--- code/settings.py
+++ code/settings.py
@@ -1,22 +1,16 @@
-# import os
-# BASE_PATH = os.getcwd()
-
-
-# def get_path(folder, file): return os.path.join(BASE_PATH, folder, file)
-
-
 # Screen settings
 SCREEN_W = 1280
 SCREEN_H = 720
 TILE_SIZE = 64
 
-TITLE = 'Betrayal at House on a Hill'
+TITLE = 'Betrayal at House on the Hill'
 
+# Paths
 BG_IMAGE = '../graphics/red_velvet_bg.jpg'
+ROOM_PATH = '../graphics/rooms/'
 
 # image sizes
-# TILE_SIZE = 200
-PLAYER_SIZE = 100
+PLAYER_SIZE = 32
 
 # colors
 WHITE = (255, 255, 255)
