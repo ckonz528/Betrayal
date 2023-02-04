619c54b4d2f085c196568f7301a33da00d9be957
Claire Konz <claire.konz@gmail.com>
2023-02-03 19:37:15 -0500

    added data paths

--- code/settings.py
+++ code/settings.py
@@ -5,9 +5,13 @@ TILE_SIZE = 64
 
 TITLE = 'Betrayal at House on the Hill'
 
-# Paths
+# Graphic Paths
 BG_IMAGE = '../graphics/red_velvet_bg.jpg'
 ROOM_PATH = '../graphics/rooms/'
+
+# Data Paths
+OMEN_JSON = '../data/omens_3e.json'
+ITEM_JSON = '../data/items_3e.json'
 
 # image sizes
 PLAYER_SIZE = 32
