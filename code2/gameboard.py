4929368d75194173855917b2dcd9422837dca39d
Claire Konz <claire.konz@gmail.com>
2023-04-16 01:19:08 -0400

    create decks

--- code2/gameboard.py
+++ code2/gameboard.py
@@ -1,6 +1,6 @@
 import pygame
 import settings as s
-from rooms import RoomDeck
+from decks import Deck, RoomDeck
 
 
 class Gameboard:
@@ -13,16 +13,24 @@ class Gameboard:
 
     def setup(self):
         # Room tiles
-        self.room_deck = RoomDeck()
+        self.room_deck = RoomDeck('../data/rooms.json', 'room')
         self.place_tile()  # test
 
         # Cards
+        self.omen_deck = Deck('../data/omens.json', 'object')
+        print(self.omen_deck.choose_card().name)  # test
+
+        self.item_deck = Deck('../data/items.json', 'object')
+        print(self.item_deck.choose_card().name)  # test
+
+        self.event_deck = Deck('../data/events.json', 'event')
+        print(self.event_deck.choose_card().name)  # test
 
         # Players
 
     def place_tile(self):
         # TODO: dynamically choose the floor
-        tile = self.room_deck.choose_tile("basement")
+        tile = self.room_deck.choose_card(floor="basement")
         print(tile.name)
 
         # place tile on screen
