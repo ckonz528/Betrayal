ad365a1eafbe804bc508c00c54faa4e3a98f70d6
Claire Konz <claire.konz@gmail.com>
2023-04-16 01:25:56 -0400

    character object init testing

--- code2/explorers.py
+++ code2/explorers.py
@@ -28,3 +28,19 @@ class Character():
         self.sanity_scale = char_info['sanity scale']
         self.sanity_base_pos = char_info['base sanity pos']
         self.sanity_pos = self.sanity_base_pos
+
+
+if __name__ == '__main__':
+    info_path = '../data/characters.json'
+    with open(info_path, "r", encoding='utf-8') as data_file:
+        info_list = json.load(data_file)
+
+    # choose what type of card objects are created
+    obj_dict = {char_info['name']: Character(
+        char_info) for char_info in info_list}
+
+    name_list = list(obj_dict.keys())
+
+    character = obj_dict[name_list[3]]
+
+    print(character.knowledge_scale[character.knowledge_pos])
